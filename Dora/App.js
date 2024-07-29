dc5d4098a09d71aa0ee6dbb06cc0187a88d2d661
matinadongol <dongolmt@gmail.com>
2024-07-29 04:00:33 -0400

    initial commit

---
+++ Dora/App.js
@@ -0,0 +1,59 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import AllPlaces from "./screens/AllPlaces";
+import AddPlaces from "./screens/AddPlaces";
+import IconButton from "./components/UI/IconButton";
+import { Colors } from "./constants/Colors";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {backgroundColor: Colors.primary500},
+            headerTintColor: Colors.gray700,
+            contentStyle: {backgroundColor: Colors.gray700}
+          }}
+        >
+          <Stack.Screen
+            name="All Places"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              title: "Your favorite Places",
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate("AddPlaces")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen 
+            name="AddPlaces" 
+            component={AddPlaces} 
+            options={{
+              title: "Add a new place"
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ Dora/screens/AddPlaces.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import PlacesList from "../components/Places/PlacesList";
+
+
+export default function AddPlaces(){
+  return (
+  <>
+    <Text>Hi</Text>
+    <PlacesList/>
+  </>
+  )
+}
